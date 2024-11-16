eb2ef9a5b928c283e88ca4ef61d4df0eb2c527ae
Deleon <delps.santos1987@gmail.com>
2024-11-16 19:33:52 -0300

    convertendo os valores

--- test_2.py
+++ test_2.py
@@ -6,13 +6,12 @@ class Moeda:
     def info(self):
         print(f"{self.tipo}: {self.valor}")
 
-    def converter(self, para):
+    def converter(self):
         # Implementação da lógica de conversão aqui
         # Exemplo simplificado:
-        taxas = {'Dolar': {'Real': 5.0, 'Euro': 0.9},
-                 'Euro': {'Real': 5.5, 'Dolar': 1.1},
-                 'Real': {'Dolar': 0.2, 'Euro': 0.18}}
-        return self.valor * taxas[self.tipo][para]
+        taxas = {
+                 'Real': {'Dolar': 5.2, 'Euro': 6.18}}
+        return self.valor * taxas[self.tipo]
 class Cofrinho:
     def __init__(self):
         
@@ -43,15 +42,18 @@ class Euro(Moeda):
 class Real(Moeda):
     pass
 
+#Função criar moeda 
 def criar_moeda():
-    tipo_moeda = input("Selecione o tipo de Moeda (1-Real, 2-Dolar, 3-Euro): ")
-    while tipo_moeda not in ('1', '2', '3'):
+    tipo_moeda = input("Selecione o tipo de Moeda \n1-Real\n2-Dolar\n3-Euro: ")
+    while tipo_moeda not in ('1', '2', '3'): #trata os valores digitados
         print("Opção inválida. Por favor, selecione 1, 2 ou 3.")
-        tipo_moeda = input("Selecione o tipo de Moeda (1-Real, 2-Dolar, 3-Euro): ")
+        tipo_moeda = input("Selecione o tipo de Moeda \n11-Real\n2-Dolar\n3-Euro: ")
 
-    tipos = {1: 'Real', 2: 'Dolar', 3: 'Euro'}
+    tipos = {1: 'Real', 2: 'Dolar', 3: 'Euro'}# Valida o tipo de moeda sem usar os if e else
+    
     valor = float(input("Digite o valor da moeda: "))
-    return tipos[int(tipo_moeda)], valor
+
+    return tipos[int(tipo_moeda)], valor #retorna o tipo e o valor da moeda criada
 
 def main():
     cofrinho = Cofrinho()
@@ -65,15 +67,15 @@ def main():
         opcao = input("Escolha uma opcao: ")
 
         if opcao == '1':
-            tipo, valor = criar_moeda()
+            tipo, valor = criar_moeda() #abre a funciao criar e retorna o tipo e o valor
             moeda = Moeda(tipo, valor)
             cofrinho.adicionar(moeda)
         elif opcao == '2':
             cofrinho.listar_moedas()
         elif opcao == '3':
-            para = input("Converter para (Real, Dolar ou Euro): ")
+            para = Real
             total = cofrinho.total_valor_convertido(para)
-            print(f"Total em {para}: {total}")
+            print(f"Total em Real: {total}")
         elif opcao == '4':
             break
         else:
